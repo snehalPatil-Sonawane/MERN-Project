5d36bb5a697b0b5ae64a9a39f5f1c913b5ab857a
Snehal Patil <snehal.patil@Eternus.local>
2019-05-24 11:50:03 +0530

    - Redux store setup - Login page completed

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from "react";
-
+import { Link } from "react-router-dom";
 const Register = () => {
   const [ formData, setFormData ] = useState({
     name: "",
@@ -12,14 +12,14 @@ const Register = () => {
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if(password !== password2){
       console.log("Passwords do not match");
     }else{
-      console.log("Formdata", formData);
-    }
+       console.log("Success", formData);
   }
+}
   return (
     <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
@@ -74,7 +74,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );

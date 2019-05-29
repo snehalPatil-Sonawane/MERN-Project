66580d9018c93769ed150bf7cc93e374e8f63be7
Snehal Patil <snehal.patil@Eternus.local>
2019-05-29 17:45:25 +0530

    - Alert popup completed

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
-const Register = () => {
+import { connect } from "react-redux";
+import {setAlert} from "../../store/actions/alert";
+import PropTypes from 'prop-types'
+
+
+const Register = ({setAlert }) => {
   const [ formData, setFormData ] = useState({
     name: "",
     email: "",
@@ -15,7 +20,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if(password !== password2){
-      console.log("Passwords do not match");
+      setAlert("Passwords do not match", "danger");
     }else{
        console.log("Success", formData);
   }
@@ -80,4 +85,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+setAlert : PropTypes.func.isRequired,
+}
+
+export default connect(null, {setAlert})(Register);
--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,7 +1,8 @@
 import {
     combineReducers
 } from "redux";
+import alert from "./alert";
 
 export default combineReducers({
-
+    alert
 })
